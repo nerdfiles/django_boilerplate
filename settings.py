2d34ac619e82620b9f775f249697c328865ebb53
Aaron Alexander <nerdfiles@gmail.com>
2011-11-04 11:59:17 -0500

    cleanup

--- settings.py
+++ settings.py
@@ -1,18 +1,60 @@
-import os
+# == ALBUM ======================================= #
+
+"""
+  Can - Future Days
+  http://open.spotify.com/track/742Qq4oImak5UxgZu25897
+"""
+
+# == PROJECT ======================================= #
+
+"""
+  @project          unitech
+  @contributor      Aaron Alexander (nerdfiles@gmail.com)
+  @contributor      Will Dawson (will@pennebaker.com)
+  @datetime         11.4.2011.11.56.a
+"""
+
+# == IMPORTS ======================================= #
+
+import os, sys, datetime
+
+
+# == UTIL ======================================= #
 
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 DIRNAME = os.path.dirname(os.path.abspath(__file__))
+
+
+# == DEVELOPMENT/DEBUGGING ======================================= #
 
 LOCAL_DEVELOPMENT = True
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+
+# == ADMIN/GENERAL ======================================= #
+
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+  ('nerdfiles', 'nerdfiles@gmail.com'),
+  ('Richard', 'richard@pennebaker.com'),
+  ('Will', 'will@pennebaker.com'),
 )
 
 MANAGERS = ADMINS
+
+TIME_ZONE = 'America/Chicago'
+
+SITE_ID = 1
+
 INTERNAL_IPS = ('127.0.0.1',)
+
+
+# == DB ======================================= #
+
+"""
+  @note If using local settings, this will be overridden.
+"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -24,83 +66,66 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
+# == LANGUAGE ======================================= #
+
 LANGUAGE_CODE = 'en-us'
 
-SITE_ID = 1
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
+# == I18N ======================================= #
+
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+
+# == LOCALIZATION ======================================= #
+
 USE_L10N = True
 
+
+# == THEME ======================================= #
+
 THEME = "unitech"
-
 THEME_DIR = os.path.join(PROJECT_ROOT, "_themes", THEME)
-
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "_themes", THEME, "templates"),
 )
+MEDIA_ROOT = os.path.realpath(os.path.join(THEME_DIR, "_assets"))
 
-MEDIA_ROOT = os.path.realpath(os.path.join(THEME_DIR, "static"))
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+# == URL ======================================= #
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+MEDIA_URL = '/_assets/'
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+# == ADMIN ======================================= #
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+ADMIN_MEDIA_PREFIX = '/_assets/admin/'
 
-# List of finder classes that know how to find static files in
-# various locations.
+
+# == STATIC ======================================= #
+
+STATICFILES_DIRS = ()
+
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+  'django.contrib.staticfiles.finders.FileSystemFinder',
+  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+  'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
+
 SECRET_KEY = 'h35nfv4bn6klt-1@dkyknvc*-h@$n7fm92$^9kc6^^6ito*jd4'
 
-# List of callables that know how to import templates from various sources.
+
+# == TEMPLATE LOADERS ======================================= #
+
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+  'django.template.loaders.filesystem.Loader',
+  'django.template.loaders.app_directories.Loader',
+  'django.template.loaders.eggs.Loader',
 )
+
+
+# == MIDDLEWARE CLASSES ======================================= #
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -110,13 +135,22 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
+
+# == URLCONF ======================================= #
+
 ROOT_URLCONF = 'unitech.urls'
+
+
+# == TEMPLATE ======================================= #
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+
+
+# == INSTALLED APPS ======================================= #
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -131,11 +165,9 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
+# == LOGGING ======================================= #
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -154,6 +186,9 @@ LOGGING = {
     }
 }
 
+
+# == CACHING ======================================= #
+
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
@@ -165,17 +200,25 @@ CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
 CACHE_TIMEOUT = 60*30
 CACHE_PREFIX = "Z"
 
+
+# == APP: PIL ======================================= #
+
 PIL_IMAGEFILE_MAXBLOCK = 1024 * 2 ** 10
 
 
-# Email Settings
+# == APP?: DJANGO-EMAIL ======================================= #
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+
+# == LOCAL ======================================= #
+
 try:
     from local_settings import *
 except ImportError:
     pass
+
