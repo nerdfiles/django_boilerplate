4f648ee29877404f108be29137b577280ff5939f
Aaron Alexander <nerdfiles@gmail.com>
2011-11-04 12:28:17 -0500

    dev

---
+++ local_settings.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# == DB ======================================== #
+
+"""
+  @engines            'postgresql_psycopg2'|'postgresql'|'mysql'|'sqlite3'|'oracle'
+"""
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'dev.db'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
+
+
+# == DEVELOPMENT/DEBUGGING ======================================== #
+
+DEBUG = True
+
+
+# == LOCAL SETTINGS ======================================== #
+
